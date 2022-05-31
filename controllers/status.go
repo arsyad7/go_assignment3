13480def8231432e4f5d6ad9b470b9792a1b00a7
arsyad7 <arsyadrama7@gmail.com>
2022-05-31 08:16:48 +0700

    fix bug

--- controllers/status.go
+++ controllers/status.go
@@ -10,16 +10,14 @@ import (
 func GetStatus(c *gin.Context) {
 	res := models.GetStatus()
 
-	var water models.Water
-	var wind models.Wind
-
-	water.Status = res.Water.Status
-	water.Message = res.Water.Message
-	wind.Message = res.Wind.Message
-	wind.Status = res.Water.Status
-
 	c.HTML(http.StatusOK, "template.html", gin.H{
-		"water": water,
-		"wind":  wind,
+		"water": models.Water{
+			Status:  res.Water.Status,
+			Message: res.Water.Message,
+		},
+		"wind": models.Wind{
+			Status:  res.Wind.Status,
+			Message: res.Wind.Message,
+		},
 	})
 }
